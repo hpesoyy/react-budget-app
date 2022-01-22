a849332af7c9572804e2c59494642b4f69ee38a0
hpesoyy <yosephyilak40@gmail.com>
2022-01-22 15:44:51 +0300

    budget context finished

--- src/contexts/BudgetsContext.js
+++ src/contexts/BudgetsContext.js
@@ -1,7 +1,55 @@
-export function useBudgets(){
+import React, { useContext, useState } from 'react'
+import {v4 as uuidv4} from 'uuid'
 
+const BudgetsContext = React.createContext()
+
+export function useBudgets(){
+    return useContext(BudgetsContext)
 }
 
-export const BudgetProvider = ({ children}) => {
-    return children
+export const BudgetsProvider = ({ children}) => {
+    const [budgets, setBudgets] = useState([])
+    const [expenses, setExpenses] = useState([])
+    
+    function getBudgetExpenses(budgetId){
+        return expenses.filter(expense => expense.budgetId === budgetId)
+    }
+    function addExpense(description, amount, budgetId){
+        setExpenses(prevExpenses =>{
+
+            return [...prevExpenses, {id: uuidv4(), description, amount, budgetId }]
+        })
+    }
+
+    function addBudget({name, max}){
+        setBudgets(prevBudgets =>{
+            if(prevBudgets.find(budget => budget.name === name)){
+                return prevBudgets
+            }
+            return [...prevBudgets, {id: uuidv4(), name, max }]
+        })
+    }
+
+    function deleteBudget({id}){
+        setBudgets(prevBudgets => {
+            return prevBudgets.filter(budget => budget.id !== id)
+        })
+    }
+    function deleteExpense({id}){
+        setExpenses(prevExpense => {
+            return prevExpense.filter(expense => expense.id !== id)
+        })
+    }
+
+    return (
+    <BudgetsContext.Provider value={{
+      budgets,
+      expenses,
+      getBudgetExpenses,
+      addExpense,
+      addBudget,
+      deleteBudget,
+      deleteExpense    
+    }}>{children}</BudgetsContext.Provider>
+    )
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css"
+import { BudgetsProvider } from "./contexts/BudgetsContext"
+import { Form } from 'react-bootstrap';
 
 ReactDOM.render(
   <React.StrictMode>
+    <BudgetsProvider>
     <App />
+    </BudgetsProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+//import { Form } from 'react-bootstrap';
 
 
